ed5e89ca35dbfc193c219e1912d4c2670d724e9c
emilshigin <emilshigin@protonmail.com>
2024-04-16 12:13:43 -0700

    added more function uninstall apk and new deviceinfo functions

--- adb_connection.py
+++ adb_connection.py
@@ -43,7 +43,6 @@ def get_device_info():
             "MAC Address" : "##:##:##:##:##:##",     
             "Bluetooth Adress" : "##:##:##:##:##:##",
         }
-
     return {
         "name" : device.shell('getprop sys.pxr.product.name').strip(),
         "Internal SN" : device.shell('getprop ro.serialno').strip(),
@@ -53,3 +52,29 @@ def get_device_info():
             ).group(),
         "Bluetooth Adress" : device.shell("settings get secure bluetooth_address").strip()
     }
+
+# Functions for testing
+def new_device_info():
+    try:    
+        device = AdbDeviceUsb()
+        device.connect(rsa_keys=[get_adb_key()], auth_timeout_s=0.1)
+    except:
+        return print("error no device found")
+    print(device.shell('getprop '))
+
+def uninstall_apk():
+    try:    
+        device = AdbDeviceUsb()
+        device.connect(rsa_keys=[get_adb_key()], auth_timeout_s=0.1)
+    except:
+        return print("error no device found")
+    print(device.shell('uninstall com.MMD.VR2KN3'))
+
+
+# List od adb commands
+# https://gist.github.com/Pulimet/5013acf2cd5b28e55036c82c91bd56d8
+# python uninstall uninstall com.MMD.VR2KN3
+# Neo2 [pxr.vendorhw.product.model]: [Pico Neo 2 Eye]
+
+# if __name__ == '__main__':
+#     uninstall_apk()
