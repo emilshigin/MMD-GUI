c9fbde1fa50504fb84c63d8af2c1da8963daade5
emilshigin <emilshigin@protonmail.com>
2024-04-16 12:11:57 -0700

    orginztion updated

--- main-tk.py
+++ main-tk.py
@@ -50,56 +50,57 @@ def content_handler(switch_to = None):
 # TODO: device info needs to update if headset is unplaged and repluged or a new headset is plugged in Use the device scan button
 # TODO: make better gui by reading pythonguis.com
 
-config_data = config_data()
+if __name__ == '__main__':
+    config_data = config_data()
 
-# Window defualts
-window = tk.Tk()
-window.title('layout')
-window.geometry('600x400')
-window.update()
+    # Window defualts
+    window = tk.Tk()
+    window.title('layout')
+    window.geometry('600x400')
+    window.update()
 
-# Frames defualts
-menu_frame_min_width = 50
-menu_frame_max_width = 100
-menu_frame_curremt_width = menu_frame_min_width
-menu_frame_expanded = False
-window_bottom_bar_hieght = 15
+    # Frames defualts
+    menu_frame_min_width = 50
+    menu_frame_max_width = 100
+    menu_frame_curremt_width = menu_frame_min_width
+    menu_frame_expanded = False
+    window_bottom_bar_hieght = 15
 
-# Sections on the window
-body_frame = tk.Frame(window, background = "blue", width = window.winfo_width() ,height=window.winfo_height()-window_bottom_bar_hieght)
-window_bottom_bar = tk.Frame(window,background="green",height=window_bottom_bar_hieght)
-content_frame = tk.Frame(body_frame,background="gray", width=window.winfo_width() , height=window.winfo_height()-window_bottom_bar_hieght)
-menu_frame =  tk.Frame(body_frame, background = "red", width = menu_frame_min_width , height=window.winfo_height()-window_bottom_bar_hieght)
+    # Sections on the window
+    body_frame = tk.Frame(window, background = "blue", width = window.winfo_width() ,height=window.winfo_height()-window_bottom_bar_hieght)
+    window_bottom_bar = tk.Frame(window,background="green",height=window_bottom_bar_hieght)
+    content_frame = tk.Frame(body_frame,background="gray", width=window.winfo_width() , height=window.winfo_height()-window_bottom_bar_hieght)
+    menu_frame =  tk.Frame(body_frame, background = "red", width = menu_frame_min_width , height=window.winfo_height()-window_bottom_bar_hieght)
 
-window.grid_rowconfigure(0, weight=1)
-window.grid_columnconfigure(0, weight=1)
+    window.grid_rowconfigure(0, weight=1)
+    window.grid_columnconfigure(0, weight=1)
 
-body_frame.grid_rowconfigure(0,weight=1)
-body_frame.grid_columnconfigure(1, weight=1)
+    body_frame.grid_rowconfigure(0,weight=1)
+    body_frame.grid_columnconfigure(1, weight=1)
 
-content_frame.grid_rowconfigure(0,weight=1)
-content_frame.grid_columnconfigure(0, weight=1)
+    content_frame.grid_rowconfigure(0,weight=1)
+    content_frame.grid_columnconfigure(0, weight=1)
 
-body_frame.grid(column=0, row = 0 , sticky="news")
-window_bottom_bar.grid(column=0, row = 1 , sticky="ews")
-menu_frame.grid(column= 0, row = 0 ,sticky="news")
-content_frame.grid(column= 1, row = 0 ,sticky="news")
+    body_frame.grid(column=0, row = 0 , sticky="news")
+    window_bottom_bar.grid(column=0, row = 1 , sticky="ews")
+    menu_frame.grid(column= 0, row = 0 ,sticky="news")
+    content_frame.grid(column= 1, row = 0 ,sticky="news")
 
-# Update Content Frame
-content_handler(config_data["start_page"])
-# content_handler() # For Tests
+    # Update Content Frame
+    content_handler(config_data["start_page"])
+    # content_handler() # For Tests
 
 
-# Menu Content
-menu_production_button = tk.Button(menu_frame, relief='flat',text="Prod", font=(0,15))
-menu_settings_button = tk.Button(menu_frame,relief='flat',text="Sett", font=(0,15))
+    # Menu Content
+    menu_production_button = tk.Button(menu_frame, relief='flat',text="Prod", font=(0,15))
+    menu_settings_button = tk.Button(menu_frame,relief='flat',text="Sett", font=(0,15))
 
-menu_production_button.grid(column=0, row = 0 , sticky="new")
-menu_settings_button.grid(column=0, row = 1 , sticky="ews")
+    menu_production_button.grid(column=0, row = 0 , sticky="new")
+    menu_settings_button.grid(column=0, row = 1 , sticky="ews")
 
-menu_frame.bind('<Enter>', lambda e: enter_menu_frame())
-menu_frame.bind('<Leave>', lambda e: leave_menu_frame())
-menu_frame.grid_propagate(False)
+    menu_frame.bind('<Enter>', lambda e: enter_menu_frame())
+    menu_frame.bind('<Leave>', lambda e: leave_menu_frame())
+    menu_frame.grid_propagate(False)
 
-#run
-window.mainloop()
+    #run
+    window.mainloop()
