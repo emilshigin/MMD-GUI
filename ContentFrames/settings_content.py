30fbbe93a9061389480cf6530b2edd5852699088
emilshigin <emilshigin@protonmail.com>
2024-06-27 17:49:54 -0700

    Move top label under settings layout

--- ContentFrames/settings_content.py
+++ ContentFrames/settings_content.py
@@ -180,17 +180,17 @@ def notebook_frame(self,settings_frame):
 def content(self,window,content_frame):
     self.window = window
 
-    # Top lable
-    settings_label_frame = tk.Frame(settings_frame)
-    settings_label_frame.grid(row=0, column=0, sticky= "ns")
-    settings_label = tk.Label(settings_label_frame,text="Settings Page",font=("Arial",25))
-    settings_label.grid(row=0, column=0)
-    
     # Settings Layout Setup
     settings_frame = tk.Frame(content_frame,bd=1,relief="solid")
     settings_frame.grid(column= 0, row= 0, sticky="news")
     settings_frame.grid_rowconfigure(2,weight=1)
     settings_frame.grid_columnconfigure(0, weight=1)
+    
+    # Top lable
+    settings_label_frame = tk.Frame(settings_frame)
+    settings_label_frame.grid(row=0, column=0, sticky= "ns")
+    settings_label = tk.Label(settings_label_frame,text="Settings Page",font=("Arial",25))
+    settings_label.grid(row=0, column=0)
 
     # Render Layout 
     option_start_up(self,settings_frame)  
