54d05fad6dbfcaebfdbc68f94d375f8f8d2a4709
emilshigin <emilshigin@protonmail.com>
2024-06-28 21:34:59 -0700

    removed useless code

--- adb_connection.py
+++ adb_connection.py
@@ -76,90 +76,28 @@ class device:
         data = json.load(open(file=CONFIG_PATH))
         
         print("Push to devices: ",device_name)
-        if(device_name == 'Pico Neo 3 Pro Eye'):
-            column = []
+        column = []
 
-            device_upload_list = list(data[device_name].keys())
-            
-            count = 0
-            for upload_list in device_upload_list:
-                if data[device_name][upload_list]['Want'] == "1":
-                    # Message: command in progress
-                    text_appened = data[device_name][upload_list]['Text']
-                    column.append(tk.Label(device_check_list, text = text_appened))
-                    column[count].grid()
-                    count += 1
+        device_upload_list = list(data[device_name].keys())
+        
+        count = 0
+        for upload_list in device_upload_list:
+            if data[device_name][upload_list]['Want'] == "1":
+                # Message: command in progress
+                text_appened = data[device_name][upload_list]['Text']
+                column.append(tk.Label(device_check_list, text = text_appened))
+                column[count].grid()
+                count += 1
 
-                    # Command implementation
-                    command = data[device_name][upload_list]['ADP command']
-                    path = '"'+data[device_name][upload_list]['Path']+'"'
+                # Command implementation
+                command = data[device_name][upload_list]['ADP command']
+                path = '"'+data[device_name][upload_list]['Path']+'"'
 
-                    device_storage_path = ""
-                    if('Device Storage Path' in data[device_name][upload_list]):
-                       device_storage_path = '"'+data[device_name][upload_list]['Device Storage Path']+'"'
+                device_storage_path = ""
+                if('Device Storage Path' in data[device_name][upload_list]):
+                    device_storage_path = '"'+data[device_name][upload_list]['Device Storage Path']+'"'
 
-                    os.system(f'adb {command} {path} {device_storage_path}')
-
-                    
-            
-            #Make Device Chacklist Label
-            
-            # pushing_da = tk.Label(device_check_list,text='Pushing DA Calib')
-            # install_am = tk.Label(device_check_list,text='Installing App Manager')
-            # install_pm = tk.Label(device_check_list,text='Installing PM APK')
-            # install_vf = tk.Label(device_check_list,text='Installing VF APK')
-            
-            # # Push Files
-            # # DA Calabration
-            # order_counter+=1
-            # pushing_da.grid(row=order_counter, column=0,sticky='we')
-            # temp_str = data["Pico Neo 3"]["Current DA Calib"]
-            # os.system(f'adb push "{temp_str}" /storage/emulated/0/Download')
-
-
-            #     # App Manager
-            # order_counter+=1
-            # install_am.grid(row=order_counter, column=0,sticky='we')
-            # temp_str = data["Pico Neo 3"]["Current App Manager APK"]
-            # os.system(f'adb push "{temp_str}" /storage/emulated/0/Download')
-            # os.system(f'adb install "{temp_str}"')
-
-            #     # Puplometer
-            # order_counter+=1
-            # install_pm.grid(row=order_counter, column=0,sticky='we')
-            # temp_str = data["Pico Neo 3"]["Current PM APK"] 
-            # os.system(f'adb push "{temp_str}" /storage/emulated/0/Download')
-            # os.system(f'adb install "{temp_str}"')
-
-            #     # VF2000
-            # order_counter+=1
-            # install_vf.grid(row=order_counter, column=0,sticky='we')
-            # temp_str = data["Pico Neo 3"]["Current VF APK"]
-            # os.system(f'adb push "{temp_str}" /storage/emulated/0/Download')
-            # os.system(f'adb install "{temp_str}"')
-
-        elif ( device_name == "PICO G3"):
-            install_am = tk.Label(device_check_list,text='Installing App Manager')
-            install_vf = tk.Label(device_check_list,text='Installing VF APK')
-
-
-            # Push Files
-            #   # App Manager
-            order_counter+=1
-            install_am.grid(row=order_counter, column=0,sticky='we')
-            temp_str = data["PICO G3"]["Current App Manager APK"] 
-            os.system(f'adb push "{temp_str}" /storage/emulated/0/Download')
-            os.system(f'adb install "{temp_str}"')
-            
-            #   # VF2000
-            order_counter+=1
-            install_vf.grid(row=order_counter, column=0,sticky='we')
-            temp_str = data["PICO G3"]["Current VF APK"] 
-            os.system(f'adb push "{temp_str}" /storage/emulated/0/Download')
-            os.system(f'adb install "{temp_str}"')
-            
-        else:
-            print("Devices No Yet Setup: ",device_name)
+                os.system(f'adb {command} {path} {device_storage_path}')
 
         # Add the last line in feedback loop
         column.append(tk.Label(device_check_list,text='All Porcesses Finished'))
